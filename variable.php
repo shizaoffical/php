be034775b7e63aabc22bd9fa80bbc8510a1b228b
shizaoffical <110219012+shizaoffical@users.noreply.github.com>
2022-08-19 16:36:41 +0500

    Create variable.php

---
+++ variable.php
@@ -0,0 +1,13 @@
+
+<?php
+//  variable is a container  that is used for storing information  
+// variable is a case sensitive
+
+// case sensitive means that  there is a difference between small ang bid alphabets
+$name = "shiza";
+$income = 10000;
+
+echo " the name is $name and his income is  $income <br>"; 
+echo  "$name is an advocate <br>";  
+echo  "$name is also a  web developer"
+?>
