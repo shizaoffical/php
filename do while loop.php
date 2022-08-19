2c5d3b4f08e1aeae1cca96242b1e0b0fd714342e
shizaoffical <110219012+shizaoffical@users.noreply.github.com>
2022-08-20 00:28:28 +0500

    update

---
+++ do while loop.php
@@ -0,0 +1,17 @@
+
+
+
+<?php
+echo  "welcome in do while loop ";
+echo  " do while loop means that this loop ....always give us a line...means that it always execute at least once ";
+$s = 77;
+     do{
+         echo "$s  <br>";
+        $s++; 
+     }while($s < 99);
+       
+
+
+
+
+?>
