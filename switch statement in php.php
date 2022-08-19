ad508588d7f1aec0393696a57e92deee77d38bd7
shizaoffical <110219012+shizaoffical@users.noreply.github.com>
2022-08-19 22:55:32 +0500

    Create switch statement in php.php

---
+++ switch statement in php.php
@@ -0,0 +1,25 @@
+
+<?php
+$marks = 50;
+
+switch($marks){
+    case 50:
+    echo "you are fail <br>"; 
+    break;
+    case  80:
+    echo "you are near to pass <br>";
+    break;
+    case  100:
+    echo "you are pass <br>";
+    break;
+    case  120:
+    echo "you are pass <br>";
+    break;
+default:
+   echo  "you are a brilliant student <br>";
+    break;
+}
+
+
+
+?>
